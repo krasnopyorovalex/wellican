1106026b3f86746e05dfedd5036cd2d04a3ed3ce
Fedor <Fedor@DESKTOP-24RJST5>
2023-12-24 21:46:49 +0300

    Блок - типы недвижимости v1.0

--- slice/app/ts/main.ts
+++ slice/app/ts/main.ts
@@ -1,3 +1,21 @@
-export interface TestClass {
-    test1: string;
+// Разукрашиваем элемент в заголовке типов недвижимсоти
+function initFillColorForTypeTitle(): void {
+    const estateTypeTitleList: NodeList =
+        document.querySelectorAll(".estate_type_title");
+
+    const firstEl = estateTypeTitleList[0] as HTMLElement;
+
+    let slitText: string[] = firstEl.innerText.trim().split(" ");
+
+    const lastWord = slitText[slitText.length - 1];
+
+    const res = slitText.slice(0, -1);
+
+    firstEl.innerHTML =
+        res.join(" ") +
+        (slitText.length > 0
+            ? " <i class='highlight'>" + lastWord + "</i>"
+            : lastWord);
 }
+
+initFillColorForTypeTitle();
