fbfad94d56bf4e8049933956952b82cc0434e846
Fedor <Fedor@DESKTOP-24RJST5>
2024-01-08 12:07:05 +0300

    Мелкие правки

--- slice/public/js/main.js
+++ slice/public/js/main.js
@@ -1,34 +1,37 @@
 // Разукрашиваем элемент в заголовке типов недвижимсоти
 function initFillColorForTypeTitle() {
-    var estateTypeTitleList = document.querySelectorAll(".estate_type_title");
-    var firstEl = estateTypeTitleList[0];
-    var slitText = firstEl.innerText.trim().split(" ");
-    var lastWord = slitText[slitText.length - 1];
-    var res = slitText.slice(0, -1);
-    firstEl.innerHTML =
-        res.join(" ") +
-            (slitText.length > 0
-                ? " <i class='highlight'>" + lastWord + "</i>"
-                : lastWord);
+    const estateTypeTitleList = document.querySelectorAll(".estate_type_title");
+    if (estateTypeTitleList.length) {
+        const firstEl = estateTypeTitleList[0];
+        let slitText = firstEl.innerText.trim().split(" ");
+        const lastWord = slitText[slitText.length - 1];
+        const res = slitText.slice(0, -1);
+        firstEl.innerHTML =
+            res.join(" ") +
+                (slitText.length > 0
+                    ? " <i class='highlight'>" + lastWord + "</i>"
+                    : lastWord);
+    }
 }
 function initCarousel() {
-    var items = document.querySelectorAll(".carousel .carousel-item");
-    items.forEach(function (el) {
-        var minPerSlide = 3;
-        var next = el.nextElementSibling;
-        for (var i = 1; i < minPerSlide; i++) {
-            if (!next) {
-                // wrap carousel by using first child
-                next = items[0];
+    let items = document.querySelectorAll(".carousel .carousel-item");
+    if (items.length) {
+        items.forEach((el) => {
+            const minPerSlide = 3;
+            let next = el.nextElementSibling;
+            for (let i = 1; i < minPerSlide; i++) {
+                if (!next) {
+                    // wrap carousel by using first child
+                    next = items[0];
+                }
+                let cloneChild = next.cloneNode(true);
+                el.appendChild(cloneChild.children[0]);
+                next = next.nextElementSibling;
             }
-            var cloneChild = next.cloneNode(true);
-            el.appendChild(cloneChild.children[0]);
-            next = next.nextElementSibling;
-        }
-    });
+        });
+    }
 }
-initFillColorForTypeTitle();
-window.addEventListener("load", function () {
-    console.log("All assets are loaded");
+(() => {
     initCarousel();
-});
+    initFillColorForTypeTitle();
+})();
