14071a3962c7d3dccf0de8e8005ce1e23a096288
Alexander Krasnopyorov <a.krasnoperov@pfsol.ru>
2024-03-19 17:28:05 +0300

    Добавил метки для объектов

---
+++ app/Http/Requests/Admin/ObjectLabel/UpdateObjectLabelRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Requests\Admin\ObjectLabel;
+
+/**
+ * @property int $id
+ */
+class UpdateObjectLabelRequest extends StoreObjectLabelRequest
+{
+
+}
--- app/View/Composers/LocationComposer.php
+++ app/View/Composers/LocationComposer.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace App\View\Composers;
 
-use App\Domain\Entities\ObjectType\Requests\ObjectTypeRequest;
 use Domain\Contracts\Persistence\Storage;
 use Domain\Entities\Location\Location;
+use Domain\Entities\Location\Requests\LocationRequest;
 use Domain\Persistence\Storage\Queries\GetAllQuery;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\View\View;
@@ -25,7 +25,7 @@ class LocationComposer
     public function compose(View $view): void
     {
         if (! isset(self::$collection)) {
-            self::$collection = $this->storage->getAll(new GetAllQuery(ObjectTypeRequest::fromArray([]), new Location()));
+            self::$collection = $this->storage->getAll(new GetAllQuery(LocationRequest::fromArray([]), new Location()));
         }
 
         $view->with('locations', self::$collection);
